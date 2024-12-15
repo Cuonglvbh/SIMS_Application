a5099db8fe5574f5e0f5d6293e6028acdf6fd869
Cuonglvbh <142555910+Cuonglvbh@users.noreply.github.com>
2024-12-15 19:53:30 +0700

    Add files via upload

---
+++ ASM2_1ST_APDP_CuongLV_BH01311/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace ASM2_1ST_APDP_CuongLV_BH01311
+{
+    public class Account
+    {
+        private string username;
+        private string password;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            set { password = value; }
+        }
+    }
+
+}
---
+++ ASM2_1ST_APDP_CuongLV_BH01311/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM2_1ST_APDP_CuongLV_BH01311
+{
+    public class Subject
+    {
+        public string SubjectCode { get; set; }
+        public string SubjectName { get; set; }
+        public string Description { get; set; }
+    }
+}
